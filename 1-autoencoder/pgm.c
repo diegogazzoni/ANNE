c488796555b9ae0d5b853ab690dd5839dcfa17bd
Diego Gazzoni <diego@MacBook-Pro-di-Diego.local>
2026-01-20 19:25:46 +0100

    pgm lib

--- 1-autoencoder/pgm.c
+++ 1-autoencoder/pgm.c
@@ -1,2 +1,23 @@
-#include <stdio.>
+#include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
+#define MAX_LEN 70
+
+typedef struct {
+	char *magic;
+	int w, h;
+	int max;
+	double *data;
+} PGM;
+
+int read(PGM* dest, char* filename) {
+
+}
+
+int main() {
+	PGM pgm;
+	//read(&pgm, "./data/baboon.ascii.pgm");
+
+	return 0;
+}
